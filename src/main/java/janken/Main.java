32666d3b9726498f6e9eaaf1889b840e0787d11d
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-08-11 00:45:31 +0900

    Modify Step1

--- src/main/java/janken/Main.java
+++ src/main/java/janken/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) {
         JankenGame janken;
 
-        janken = new Step0();
-//        janken = new Step1();
+//        janken = new Step0();
+        janken = new Step1();
 
         System.out.println(janken.getClass().getSimpleName());
         janken.execute();
--- src/main/java/janken/step1/Step1.java
+++ src/main/java/janken/step1/Step1.java
@@ -13,37 +13,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * 第一ステップ、「１つのメソッドにつきインデントは１段階までにすること」を適用.
+ * 第１ステップ、「１つのメソッドにつきインデントは１段階までにすること」を適用.
  */
 public class Step1 implements JankenGame {
     /**ランダムな値を生成するための {@link Random} インスタンス*/
     private static final Random RANDOM = new Random(System.currentTimeMillis());
-    /**入力が不正であったことを表す値*/
-    private static final int INVALID_HAND = -1;
     /**グー*/
     private static final int ROCK = 1;
     /**チョキ*/
     private static final int SCISSORS = 2;
     /**パー*/
     private static final int PAPER = 3;
-    /**出し手ごとの表示名を保持したマップ*/
-    private static final Map<Integer, String> HAND_DISPLAY_NAME_MAP;
     /**ユーザーの主語*/
     private static final String USER_SUBJECT = "あなた";
     /**コンピュータの主語*/
     private static final String COMPUTER_SUBJECT = "わたし";
+    /**グーの表示名*/
+    private static final String DISPLAY_NAME_ROCK = "グー";
+    /**チョキの表示名*/
+    private static final String DISPLAY_NAME_SCISSORS = "チョキ";
+    /**パーの表示名*/
+    private static final String DISPLAY_NAME_PAPER = "パー";
     /**出し手が規定の値かどうかを判定するための正規表現パターン*/
     private static final Pattern HAND_REGEXP_PATTERN = Pattern.compile("[" + ROCK + SCISSORS + PAPER + "]");
-
-    static {
-        Map<Integer, String> map = new HashMap<>();
-
-        map.put(ROCK, "グー");
-        map.put(SCISSORS, "チョキ");
-        map.put(PAPER, "パー");
-
-        HAND_DISPLAY_NAME_MAP = Collections.unmodifiableMap(map);
-    }
 
     /**
      * じゃんけんプログラムを開始する.
@@ -51,93 +43,68 @@ public class Step1 implements JankenGame {
     @Override
     public void execute() {
         try (BufferedReader standardInput = new BufferedReader(new InputStreamReader(System.in))) {
-            this.processMainInfinitely(standardInput);
+            this.executeMainProcessInfinitely(standardInput);
         } catch (IOException e) {
             e.printStackTrace(System.err);
         }
     }
 
     /**
-     * メインの処理を無限ループしながら実行する.
-     * @param standardInput 標準入力をラップした {@link BufferedReader}
+     * メインのじゃんけん処理を、無限ループしながら実行する.
+     * @param standardInput 標準入力をラップした {@link BufferedReader}.
      * @throws IOException 入力エラーが発生した場合.
      */
-    private void processMainInfinitely(BufferedReader standardInput) throws IOException {
+    private void executeMainProcessInfinitely(BufferedReader standardInput) throws IOException {
         while (true) {
-            mainProcess(standardInput);
+            this.mainProcess(standardInput);
         }
     }
 
     /**
-     * じゃんけんプログラムのメインの処理
-     * @param standardInput 標準入力をラップした {@link BufferedReader}
+     * メインのじゃんけん処理.
+     * @param standardInput 標準入力をラップした {@link BufferedReader}.
      * @throws IOException 入力エラーが発生した場合.
      */
     private void mainProcess(BufferedReader standardInput) throws IOException {
-        int usersHand = this.readUsersHand(standardInput);
-
-        if (usersHand != INVALID_HAND) {
-            this.printHandDescription(USER_SUBJECT, usersHand);
-
-            int computersHand = this.readComputersHand();
-            this.printHandDescription(COMPUTER_SUBJECT, computersHand);
-
-            this.printResult(usersHand, computersHand);
-        }
-    }
-
-    /**
-     * 標準入力からユーザーの出し手の入力を取得し、 {@code int} 型に変換して返す.
-     * @param standardInput 標準入力をラップした {@link BufferedReader}
-     * @return ユーザーの入力した出し手.<br>
-     *         入力値が不正であった場合は、{@link #INVALID_HAND} の値が返される.
-     * @throws IOException 入力エラーが発生した場合.
-     */
-    private int readUsersHand(BufferedReader standardInput) throws IOException {
         System.out.println(
-            "[" + ROCK + "]" + HAND_DISPLAY_NAME_MAP.get(ROCK) + "、" +
-            "[" + SCISSORS + "]" + HAND_DISPLAY_NAME_MAP.get(SCISSORS) + "、" +
-            "[" + PAPER + "]" + HAND_DISPLAY_NAME_MAP.get(PAPER) + "を入力して下さい ⇒ "
+            "[" + ROCK + "]" + DISPLAY_NAME_ROCK + "、" +
+            "[" + SCISSORS + "]" + DISPLAY_NAME_SCISSORS + "、" +
+            "[" + PAPER + "]" + DISPLAY_NAME_PAPER + "を入力して下さい ⇒ "
         );
 
         String userInput = standardInput.readLine();
-
-        return this.isValidInput(userInput) ? Integer.parseInt(userInput) : INVALID_HAND;
-    }
-
-    /**
-     * ユーザーの入力値が有効かどうかを判定する.
-     * @param userInput ユーザーの入力値
-     * @return 有効な場合は true
-     */
-    private boolean isValidInput(String userInput) {
         Matcher matcher = HAND_REGEXP_PATTERN.matcher(userInput);
-        return matcher.matches();
-    }
 
-    /**
-     * コンピュータの出し手を取得する.
-     * @return コンピュータの出し手.
-     */
-    private int readComputersHand() {
-        return RANDOM.nextInt(3) + 1;
-    }
+        if (!matcher.matches()) {
+            return;
+        }
 
-    /**
-     * 出し手について説明するメッセージを出力する.
-     * @param subject 主語
-     * @param hand 出し手
-     */
-    private void printHandDescription(String subject, int hand) {
-        System.out.println(subject + "が出したのは「" + HAND_DISPLAY_NAME_MAP.get(hand) + "」です");
-    }
+        int usersHand = Integer.parseInt(userInput);
 
-    /**
-     * ユーザーとコンピュータの出し手を比較して、勝負の結果を出力する.
-     * @param usersHand ユーザーの出し手
-     * @param computersHand コンピュータの出し手
-     */
-    private void printResult(int usersHand, int computersHand) {
+        String handDisplayName;
+
+        if (usersHand == ROCK) {
+            handDisplayName = DISPLAY_NAME_ROCK;
+        } else if (usersHand == SCISSORS) {
+            handDisplayName = DISPLAY_NAME_SCISSORS;
+        } else {
+            handDisplayName = DISPLAY_NAME_PAPER;
+        }
+
+        System.out.println(USER_SUBJECT + "が出したのは「" + handDisplayName + "」です");
+
+        int computersHand = RANDOM.nextInt(3) + 1;
+
+        if (computersHand == ROCK) {
+            handDisplayName = DISPLAY_NAME_ROCK;
+        } else if (computersHand == SCISSORS) {
+            handDisplayName = DISPLAY_NAME_SCISSORS;
+        } else {
+            handDisplayName = DISPLAY_NAME_PAPER;
+        }
+
+        System.out.println(COMPUTER_SUBJECT + "が出したのは「" + handDisplayName + "」です");
+
         if (usersHand == computersHand) {
             System.out.println("あいこです。");
         } else {
@@ -146,9 +113,11 @@ public class Step1 implements JankenGame {
     }
 
     /**
-     * ユーザーとコンピュータの出し手を比較して、勝者を示すメッセージを出力する.
+     * ユーザーとコンピュータの出し手を比較して、勝者の情報を出力する.
      * <p>
-     * このメソッドは、あいこ以外のケースで実行されることを前提としています.
+     * このメソッドはあいこ以外で使用されることを前提としています.
+     * @param usersHand ユーザーの出し手
+     * @param computersHand コンピュータの出し手
      */
     private void printWinner(int usersHand, int computersHand) {
         String winner;
